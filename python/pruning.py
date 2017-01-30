da3bdea7089f0efed6c360d1e4a5f089048a42bb
山田　潤 <junyamada107@gmail.com>
2017-01-30 18:03:12 +0100

    output a plot

--- python/pruning.py
+++ python/pruning.py
@@ -2,6 +2,16 @@ import monkdata as m
 import dtree as d
 import random
 from  drawtree_qt5 import *
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+
+#plot settings
+plt.style.use("ggplot")
+font = {"family": "meiryo"}
+matplotlib.rc("font", **font)
+params = {"legend.fontsize": 6}
+plt.rcParams.update(params)
 
 def partition(data, fraction):
     ldata = list(data)
@@ -28,6 +38,14 @@ def prune(data, fraction):
     return current
 
 fractions = [0.3,0.4,0.5,0.6,0.7,0.8]
+result = []
 for x in fractions:
     prunedTree = prune(m.monk3,x);
-    print("Test for fraction ", x, ": ", d.check(prunedTree, m.monk3test))
+    fit = d.check(prunedTree, m.monk3test)
+    result.append(fit)
+    print("Test for fraction ", x, ": ", fit)
+plt.plot(fractions, result)
+plt.xlabel("fractions")
+plt.ylabel("fit(%)")
+plt.savefig("../plots/assignment7_monk3.png")
+
